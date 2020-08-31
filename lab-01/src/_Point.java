247ec08bb7eaadddd13651a37540a24c7b430d41
HeshamTB <hishaminv@gmail.com>
2020-08-31 12:03:22 +0300

    Point class

---
+++ lab-01/src/_Point.java
@@ -0,0 +1,37 @@
+public class _Point {
+
+    private int x;
+    private int y;
+
+    public _Point(int x, int y){
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public _Point(){
+
+        this.x = 0;
+        this.y = 0;
+
+    }
+
+    public void setLocation(int x, int y){
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public int getX(){ return x; }
+
+    public int getY(){ return y; }
+
+    public void setX(int x){ this.x = x; }
+
+    public void setY(int y){ this.y = y; }
+
+    public String toString(){ return String.format("(%d, %d)", this.x, this.y); }
+
+}
