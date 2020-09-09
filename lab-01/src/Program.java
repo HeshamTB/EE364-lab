f4fa262bd9c675c3b98a5b891187cfc7c2d654f8
HeshamTB <hishaminv@gmail.com>
2020-09-09 22:47:44 +0300

    update: 	- ignore meta-inf 	- Change c style args array in main. (recommended by IDE) 	- build with java 1.8.x

--- lab-01/src/Program.java
+++ lab-01/src/Program.java
@@ -1,7 +1,7 @@
 import java.io.PrintStream;
 
 public class Program {
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         PrintStream out = System.out;
 
