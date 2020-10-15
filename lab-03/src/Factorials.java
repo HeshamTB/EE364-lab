ad85cbea6c2942f2fe8e2628c6a4fe00acec62a0
HeshamTB <hishaminv@gmail.com>
2020-10-15 06:26:53 +0300

    lab-03: 	Basic operation of Factorial Q

---
+++ lab-03/src/Factorials.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Factorials{
+    public static void main(String args[]){
+
+        String keepGoing = "y";
+        Scanner scan = new Scanner(System.in);
+
+        while (keepGoing.equals("y") || keepGoing.equals("Y")){
+
+            System.out.print("Enter an integer: ");
+            int val = scan.nextInt();
+            System.out.println("Factorial(" + val + ") = " + factorial(val));
+
+            System.out.print("Another factorial? (y/n) ");
+            keepGoing = scan.next();
+        }
+    }
+
+    private static int factorial(int value){
+        
+        for (int i = value - 1; i > 0; i--){
+            value *= i;
+        }
+        return value;
+    }
+}
